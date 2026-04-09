3e14f3f02348d38b09b1e50f0d308e22e7281ccf
Kenric <kenmuana1@gmail.com>
2026-04-09 20:04:53 +0800

    Added Register View mockup

---
+++ src/main/java/com/group8/csit228capstone/RegisterApplication.java
@@ -0,0 +1,27 @@
+package com.group8.csit228capstone;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RegisterApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load the Register view
+        Parent root = FXMLLoader.load(getClass().getResource("register-view.fxml"));
+
+        Scene scene = new Scene(root, 600, 400);
+
+        primaryStage.setTitle("Register - CSIT228 Capstone");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
