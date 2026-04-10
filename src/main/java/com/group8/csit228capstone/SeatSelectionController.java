6452dbe995c3f9fc99c9b43ee04ace0a8d7f3c51
mis-chief <juliannamarie.cabizares@cit.edu>
2026-04-10 20:24:55 +0800

    Added Seat Selection.

---
+++ src/main/java/com/group8/csit228capstone/SeatSelectionController.java
@@ -0,0 +1,74 @@
+package com.group8.csit228capstone;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class SeatSelectionController {
+    @FXML
+    private Button seatA1;
+    @FXML
+    private Button seatB1;
+    @FXML
+    private Button seatC1;
+    @FXML
+    private Button seatD1;
+    @FXML
+    private Button seatE1;
+    @FXML
+    private Button seatA2;
+    @FXML
+    private Button seatB2;
+    @FXML
+    private Button seatC2;
+    @FXML
+    private Button seatD2;
+    @FXML
+    private Button seatE2;
+    @FXML
+    private Button seatA3;
+    @FXML
+    private Button seatB3;
+    @FXML
+    private Button seatC3;
+    @FXML
+    private Button seatD3;
+    @FXML
+    private Button seatE3;
+    @FXML
+    private Button seatA4;
+    @FXML
+    private Button seatB4;
+    @FXML
+    private Button seatC4;
+    @FXML
+    private Button seatD4;
+    @FXML
+    private Button seatE4;
+    @FXML
+    private Button seatA5;
+    @FXML
+    private Button seatB5;
+    @FXML
+    private Button seatC5;
+    @FXML
+    private Button seatE5;
+    @FXML
+    private Button seatD5;
+    @FXML
+    private Label textTotal;
+    @FXML
+    private Button btnConfirm;
+    @FXML
+    private Button btnBack;
+    @FXML
+    public void handleSeatSelection(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void handleConfirm(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void handleBack(ActionEvent actionEvent) {
+    }
+}
