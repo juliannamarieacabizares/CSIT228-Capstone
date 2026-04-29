513582c65b67efab7326dbd0b812b20cdc61f947
mis-chief <juliannamarie.cabizares@cit.edu>
2026-04-29 19:15:59 +0800

    Cabizares - Added login validation and scene switching

--- src/main/java/com/group8/csit228capstone/LoginController.java
+++ src/main/java/com/group8/csit228capstone/LoginController.java
@@ -1,10 +1,23 @@
 package com.group8.csit228capstone;
 
+import database.DatabaseConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LoginController {
     @FXML
@@ -16,5 +29,49 @@ public class LoginController {
 
     @FXML
     public void handleLogin(ActionEvent actionEvent) {
+        String email = textUsername.getText();
+        String password = textPassword.getText();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, "Form Error!", "Please enter your email and password.");
+            return;
+        }
+
+        if (validateLogin(email, password)) {
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
+                Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Event Ticketing System - Home");
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            showAlert(Alert.AlertType.ERROR, "Login Failed", "Invalid Email or Password.");
+        }
+    }
+
+    private boolean validateLogin(String email, String password) {
+        String query = "SELECT * FROM users WHERE email = ? AND password = ?";
+
+        try (Connection conn = DatabaseConnection.getInstance().getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, email);
+            pstmt.setString(2, password);
+            ResultSet resultSet = pstmt.executeQuery();
+            return resultSet.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
